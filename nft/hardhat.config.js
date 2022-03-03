adfb395ef3dd66b13e1344576308bca6671aed68
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2022-03-02 22:24:14 -0600

    Working config

--- nft/hardhat.config.js
+++ nft/hardhat.config.js
@@ -5,13 +5,20 @@ require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
 const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
-   solidity: "0.7.3",
+   solidity: {
+      version:  "0.8.1",
+      overrides: {
+        "contracts/utils/Address.sol": {
+          version: "0.8.1",
+        },
+      },
+    },
    defaultNetwork: "ropsten",
    networks: {
       hardhat: {},
       ropsten: {
          url: API_URL,
          accounts: [`0x${PRIVATE_KEY}`]
-      }
+      },
    },
 }
